f98852e2a5fc0752302e8a7f1501f110da170303
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2022-12-05 16:53:53 +0100

    update brand

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,10 +43,10 @@ export default function Home() {
         <meta name="description" content="A Cardano dApp powered my Mesh" />
         <link
           rel="icon"
-          href="https://mesh.martify.io/favicon/favicon-32x32.png"
+          href="https://meshjs.dev/favicon/favicon-32x32.png"
         />
         <link
-          href="https://mesh.martify.io/css/template.css"
+          href="https://meshjs.dev/css/template.css"
           rel="stylesheet"
           key="mesh-demo"
         />
@@ -54,7 +54,7 @@ export default function Home() {
 
       <main className="main">
         <h1 className="title">
-          <a href="https://mesh.martify.io/">Mesh</a> Multi-sig Minting
+          <a href="https://meshjs.dev/">Mesh</a> Multi-sig Minting
         </h1>
 
         <div className="demo">
@@ -78,7 +78,7 @@ export default function Home() {
         </div>
 
         <div className="grid">
-          <a href="https://mesh.martify.io/apis" className="card">
+          <a href="https://meshjs.dev/apis" className="card">
             <h2>Documentation</h2>
             <p>
               Our documentation provide live demos and code samples; great
@@ -87,7 +87,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://mesh.martify.io/guides/multisig-minting"
+            href="https://meshjs.dev/guides/multisig-minting"
             className="card"
           >
             <h2>Multi-sig minting guide</h2>
@@ -97,7 +97,7 @@ export default function Home() {
             </p>
           </a>
 
-          <a href="https://mesh.martify.io/react" className="card">
+          <a href="https://meshjs.dev/react" className="card">
             <h2>React components</h2>
             <p>
               Useful React UI components and hooks, seamlessly integrate them
